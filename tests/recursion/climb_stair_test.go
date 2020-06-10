a45f67c6427c53718e8f4195aadd5f52a906a18f
Yazan Diranieh <yazanmd@yahoo.com>
2020-06-10 20:56:56 +0100

    recursion - climb stairs unit tests

--- tests/recursion/climb_stair_test.go
+++ tests/recursion/climb_stair_test.go
@@ -1,1 +1,25 @@
 package recursion
+
+import (
+	"AlgorizmiGo/recursion"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestClimbStairs(t *testing.T) {
+	tests := []struct {
+		stepsCount int
+		expectedNumberOfWays int
+	} {
+		{stepsCount: 1, expectedNumberOfWays: 1},
+		{2, 2, },
+		{3, 3, },
+		{4, 5, },
+		{5, 8, },
+		{6, 13, },
+	}
+	for _, test := range tests {
+		actualCountOfWays := recursion.ClimbStairs(test.stepsCount)
+		assert.Equal(t, test.expectedNumberOfWays, actualCountOfWays)
+	}
+}
