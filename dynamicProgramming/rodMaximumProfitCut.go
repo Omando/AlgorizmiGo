93a58f3fd0cf1f95821ec896e77e69561ae807aa
Yazan Diranieh <yazanmd@yahoo.com>
2020-08-08 23:05:28 +0100

    Dynamic programming - matrix chain product - additional testing

--- dynamicProgramming/rodMaximumProfitCut.go
+++ dynamicProgramming/rodMaximumProfitCut.go
@@ -11,9 +11,9 @@ func RodMaximumProfitCut(prices []int, rodLength int) int {
 	var subProblems = make([]int, rodLength+1)
 	subProblems[0] = 0 // A rod of length 0 has a price of 0
 
-	// Code is deduced directly from the state-action-destination table on
-	// page 26 (DSALG_RecursionDP). Note the use of state and destination indices
-	// that relate directly to state and destination columns and their values
+	// Code is deduced directly from the state-action-destination table on page 26
+	// (DSALG_DynamicProgramming). Note the use of state and destination indices that
+	// relate directly to state and destination columns and their values
 	for state := 1; state <= rodLength; state++ {
 		for destination := 0; destination < state; destination++ {
 			length := state - destination
