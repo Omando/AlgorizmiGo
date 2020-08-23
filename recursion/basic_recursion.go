6b265843feeaa196b6feabcdc4dbd4759cda6334
Yazan Diranieh <yazanmd@yahoo.com>
2020-08-23 12:21:55 +0100

    recursion with data caches - queues and stacks

--- recursion/basic_recursion.go
+++ recursion/basic_recursion.go
@@ -1,6 +1,9 @@
 package recursion
 
-import "fmt"
+import (
+	"AlgorizmiGo/collections/sequential"
+	"fmt"
+)
 
 func StackUnwind(n int) {
 	// Exit condition
@@ -13,6 +16,103 @@ func StackUnwind(n int) {
 	fmt.Printf("After recursion - %v\n", n)
 }
 
+var preQueue sequential.Queue = sequential.CreateQueue()
+var postQueue sequential.Queue = sequential.CreateQueue()
+var preStack sequential.Stack = sequential.CreateStack(5)
+var postStack sequential.Stack = sequential.CreateStack(5)
+
 func RecursionWithDataCaches(n int) {
-	// TODO
+	doRecursionWithDataCaches(n)
+	dumpRecursionCaches()
+}
+
+func doRecursionWithDataCaches(n int) {
+	// Exit condition
+	if n == 0 {
+		return
+	}
+
+	// Cache data before recursion
+	preQueue.Enqueue(n)
+	preStack.Push(sequential.Item{n})
+
+	doRecursionWithDataCaches(n - 1)
+
+	// Cache data after recursion
+	postQueue.Enqueue(n)
+	postStack.Push(sequential.Item{n})
+}
+
+func dumpRecursionCaches() {
+	fmt.Println("PreStack:")
+	for !preStack.IsEmpty() {
+		value, err := preStack.Pop()
+		if err == nil {
+			fmt.Printf("%v ", value)
+		}
+	}
+
+	fmt.Println("\nPreQueue:")
+	for !preQueue.IsEmpty() {
+		value, err := preQueue.Dequeue()
+		if err == nil {
+			fmt.Printf("%v ", value)
+		}
+	}
+
+	fmt.Println("\nPostStack:")
+	for !postStack.IsEmpty() {
+		value, err := postStack.Pop()
+		if err == nil {
+			fmt.Printf("%v ", value)
+		}
+	}
+
+	fmt.Println("\nPostQueue:")
+	for !postQueue.IsEmpty() {
+		value, err := postQueue.Dequeue()
+		if err == nil {
+			fmt.Printf("%v ", value)
+		}
+	}
+}
+
+type incrementer func(index int) int
+
+func ForwardIterationViaRecursion(start int, end int, increment int) {
+	if start > end {
+		return
+	}
+
+	var funcIncrementer incrementer = func(index int) int { return index + increment }
+	doForwardIteration(start, end, funcIncrementer)
+}
+
+func doForwardIteration(index int, end int, funcIncrementer incrementer) {
+	// Exit condition
+	if index >= end {
+		return
+	}
+
+	fmt.Println("Processing index ", index)
+	doForwardIteration(funcIncrementer(index), end, funcIncrementer)
+}
+
+func ReverseIterationViaRecursion(start int, end int, decrement int) {
+	if start < end {
+		return
+	}
+
+	var funcDecrementer incrementer = func(index int) int { return index - decrement }
+	doReverseIteration(start, end, funcDecrementer)
+}
+
+func doReverseIteration(index int, end int, funcDecrementer incrementer) {
+	// Exit condition
+	if index <= end {
+		return
+	}
+
+	fmt.Println("Processing index ", index)
+	doForwardIteration(funcDecrementer(index), end, funcDecrementer)
 }
