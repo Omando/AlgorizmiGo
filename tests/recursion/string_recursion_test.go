dad9a25f32fdacdc23d39d4e82b13abda6c10356
Yazan Diranieh <yazanmd@yahoo.com>
2020-08-25 22:22:39 +0100

    string recursion - remove vowels and reverse string - unit tests

--- tests/recursion/string_recursion_test.go
+++ tests/recursion/string_recursion_test.go
@@ -2,6 +2,7 @@ package recursion
 
 import (
 	"AlgorizmiGo/recursion"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -23,5 +24,44 @@ func TestGetCharacterCount(t *testing.T) {
 	for _, input := range inputs {
 		actualCount := recursion.GetCharacterCount(input.input, input.char)
 		assert.Equal(t, input.expectedCount, actualCount)
+	}
+}
+
+func TestRemoveVowels(t *testing.T) {
+	tests := []struct {
+		input  string
+		output string
+	}{
+		{"", ""},
+		{"bcdf", "bcdf"},
+		{"abcdefg", "bcdfg"},
+		{"iiisssooo", "sss"},
+		{"aeiou", ""},
+	}
+
+	for _, test := range tests {
+		actualOutput := recursion.RemoveVowels(test.input)
+		assert.Equal(t, test.output, actualOutput)
+	}
+}
+
+func TestReverseString(t *testing.T) {
+	const myString = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+	fmt.Printf("%q\n", myString)
+
+	tests := []struct {
+		input  string
+		output string
+	}{
+		{"", ""},
+		{"bcdf", "fdcb"},
+		{"abcdefg", "gfedcba"},
+		{"qwerty", "ytrewq"},
+		{"yazan diranieh", "heinarid nazay"},
+	}
+
+	for _, test := range tests {
+		actualOutput := recursion.ReverseString(test.input)
+		assert.Equal(t, test.output, actualOutput)
 	}
 }
