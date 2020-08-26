1b13dfefb1b5410965166c0266145aca3c28fd59
Yazan Diranieh <yazanmd@yahoo.com>
2020-08-27 00:46:46 +0100

    string recursion - all string combinations

--- recursion/string_recursion.go
+++ recursion/string_recursion.go
@@ -1,6 +1,8 @@
 package recursion
 
-import "strings"
+import (
+	"strings"
+)
 
 // Counts how many times a given character appears in a given string
 func GetCharacterCount(s string, c byte) int {
@@ -54,6 +56,7 @@ func doRemoveVowels(s string, index int, builder *strings.Builder) string {
 	return doRemoveVowels(s, index+1, builder)
 }
 
+// Return a reversed string
 func ReverseString(s string) string {
 	if len(s) == 0 {
 		return ""
@@ -82,3 +85,44 @@ func doReverseString(chars []byte, index int) {
 	// Recursion
 	doReverseString(chars, index+1)
 }
+
+func AllStringCombinations(input string) []string {
+
+	var runes = []rune(input)		// Convert input string to an array of runes
+	var combinationSet = make(map[string]bool)
+
+	doAllStringCombinations(runes, combinationSet)
+
+	// Collect keys into a slice
+	var combinations []string = make([]string, len(combinationSet))
+	index := 0
+	for k,_ := range combinationSet {
+		combinations[index] = k
+		index++
+	}
+	return combinations
+}
+
+func doAllStringCombinations(runes []rune, combinationSet map[string]bool) {
+	// Exit condition
+	if len(runes) == 0 {
+		return
+	}
+
+	// Convert rune array to a string and add to set of combinations
+	combination := string(runes)
+	if !combinationSet[combination] {
+		combinationSet[combination] = true
+	}
+
+	// Iterate over each character in the string, omitting the current one and
+	// repeating the process
+	for i := 0; i < len(runes); i++ {
+		// Omit current character and get new combinations for the new string
+		var tempRune []rune = make([]rune, len(runes))
+		copy(tempRune, runes)
+		tempRune = append(tempRune[:i], tempRune[i+1:]...)
+		doAllStringCombinations(tempRune, combinationSet);
+	}
+}
+
