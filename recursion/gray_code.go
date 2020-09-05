e0357a3a37e4096d70c3cd27a16efea64b33f15b
Yazan Diranieh <yazanmd@yahoo.com>
2020-09-06 00:28:28 +0100

    general recursion - gray code

---
+++ recursion/gray_code.go
@@ -0,0 +1,36 @@
+package recursion
+
+func GrayCode(n int) []string {
+	var codes []string
+
+	gray(n-1, &[]byte{'0'}, &codes)
+	yarg(n-1, &[]byte{'1'}, &codes)
+
+	return codes
+}
+
+func gray(n int, code *[]byte, codes *[]string) {
+	// Exit conditions
+	if n == 0 {
+		*codes = append(*codes, string(*code))
+		return
+	}
+
+	code0 := append(*code, '0')
+	gray(n-1, &code0, codes)
+	code1 := append(*code, '1')
+	yarg(n-1, &code1, codes)
+}
+
+func yarg(n int, code *[]byte, codes *[]string) {
+	// Exit conditions
+	if n == 0 {
+		*codes = append(*codes, string(*code))
+		return
+	}
+
+	code0 := append(*code, '1')
+	gray(n-1, &code0, codes)
+	code1 := append(*code, '0')
+	yarg(n-1, &code1, codes)
+}
