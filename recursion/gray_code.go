5c4d85c97b63533296ee80c7b35a9b8bdf5a6722
Yazan Diranieh <yazanmd@yahoo.com>
2020-09-06 00:31:01 +0100

    general recursion - gray code - no need to pass code as a pointer

--- recursion/gray_code.go
+++ recursion/gray_code.go
@@ -3,34 +3,34 @@ package recursion
 func GrayCode(n int) []string {
 	var codes []string
 
-	gray(n-1, &[]byte{'0'}, &codes)
-	yarg(n-1, &[]byte{'1'}, &codes)
+	gray(n-1, []byte{'0'}, &codes)
+	yarg(n-1, []byte{'1'}, &codes)
 
 	return codes
 }
 
-func gray(n int, code *[]byte, codes *[]string) {
+func gray(n int, code []byte, codes *[]string) {
 	// Exit conditions
 	if n == 0 {
-		*codes = append(*codes, string(*code))
+		*codes = append(*codes, string(code))
 		return
 	}
 
-	code0 := append(*code, '0')
-	gray(n-1, &code0, codes)
-	code1 := append(*code, '1')
-	yarg(n-1, &code1, codes)
+	code0 := append(code, '0')
+	gray(n-1, code0, codes)
+	code1 := append(code, '1')
+	yarg(n-1, code1, codes)
 }
 
-func yarg(n int, code *[]byte, codes *[]string) {
+func yarg(n int, code []byte, codes *[]string) {
 	// Exit conditions
 	if n == 0 {
-		*codes = append(*codes, string(*code))
+		*codes = append(*codes, string(code))
 		return
 	}
 
-	code0 := append(*code, '1')
-	gray(n-1, &code0, codes)
-	code1 := append(*code, '0')
-	yarg(n-1, &code1, codes)
+	code0 := append(code, '1')
+	gray(n-1, code0, codes)
+	code1 := append(code, '0')
+	yarg(n-1, code1, codes)
 }
