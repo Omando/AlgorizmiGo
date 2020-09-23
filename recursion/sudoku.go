52d92d4a4705a4745e3d3aec19f1dbce9270358a
Yazan Diranieh <yazanmd@yahoo.com>
2020-09-23 23:58:38 +0100

    general recursion - sudoku - skeletal code

--- recursion/sudoku.go
+++ recursion/sudoku.go
@@ -1,7 +1,20 @@
 package recursion
 
+import "fmt"
+
 func Sudoku(grid [][]int) [][]int {
+	// We don't want to write to the input, so deep-copy grid into result
 	var result [][]int
+	for i, _ := range grid {
+		result[i] = make([]int, len(grid[i])) // Create a new row
+		copy(result[i], grid[i])              // Initialize new row with data
+	}
+
+	// What about
+	result2 := grid // value types so creates a copy
+	grid[0][0] = 111
+	result2[0][0] = 222
+	fmt.Println(result2)
 
 	// TODO
 
