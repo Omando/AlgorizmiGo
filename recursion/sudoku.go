255bb38f5c58f286751b896668711a17b658f5a5
Yazan Diranieh <yazanmd@yahoo.com>
2020-09-28 23:53:05 +0100

    general recursion - sudoku - delta change

--- recursion/sudoku.go
+++ recursion/sudoku.go
@@ -6,7 +6,7 @@ func Sudoku(grid [][]int) [][]int {
 	// to be reflected in the other
 	//	var result [][]int = grid
 	var result [][]int = make([][]int, len(grid))
-	for i, _ := range grid {
+	for i := range grid {
 		result[i] = make([]int, len(grid[i])) // Create a new row
 		copy(result[i], grid[i])              // Initialize new row with data
 	}
@@ -16,7 +16,26 @@ func Sudoku(grid [][]int) [][]int {
 	return result
 }
 
-func canAddItem(grid [][]int, row int, column int, misisngNumber int) bool {
+func solve(i int, j int, cells [][]int) bool {
+	// Check for exit conditions
+	if i == 3 {
+		i = 0
+		j = j + 1
+		if j == 3 {
+			return true
+		}
+	}
+
+	// Skip if cell is already populated
+	// todo
+
+	// calculation
+	// todo
+
+	return true
+}
+
+func canAddItem(grid [][]int, row int, column int, number int) bool {
 	// todo
 	return false
 }
