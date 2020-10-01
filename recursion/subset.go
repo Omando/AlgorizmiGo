63c3251f1232a54c7489f37f0f1992b87ee59a44
Yazan Diranieh <yazanmd@yahoo.com>
2020-10-01 15:54:27 +0100

    subset sum via recursion

--- recursion/subset.go
+++ recursion/subset.go
@@ -1,28 +1,5 @@
 package recursion
 
-/* Given an array of non-consecutive numbers and a target value, find whether
-there exists a subset of those numbers that sums up to the target value*/
-func SubsetSum(input []int, targetSum int) bool {
-	return doSubSetSum(input, targetSum, 0, 0)
-}
-
-func doSubSetSum(input []int, targetSum int, index int, currentSum int) bool {
-	if currentSum == targetSum {
-		return true
-	}
-
-	if currentSum > targetSum || index == len(input) {
-		return false
-	}
-
-	var hasTargetSum = doSubSetSum(input, targetSum, index+1, currentSum+input[index])
-	if hasTargetSum == false {
-		hasTargetSum = doSubSetSum(input, targetSum, index+1, currentSum)
-	}
-
-	return hasTargetSum
-}
-
 func Subset(input string) []string {
 	// Convert input string to a rune
 	inputAsRunes := []rune(input)
