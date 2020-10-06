a7ace65329e6fe7ce63718d27c9c798f473fb64f
Yazan Diranieh <yazanmd@yahoo.com>
2020-10-06 23:43:41 +0100

    path finder via recursion - movement enum

--- recursion/path_finder.go
+++ recursion/path_finder.go
@@ -1,1 +1,22 @@
 package recursion
+
+type  Point struct {
+	x int
+	y int
+}
+
+// Declare an enum to represent all possible movements from the current cell
+type Movement int		// Used to declare an enum
+const (
+	NONE Movement = iota
+	LEFT
+	RIGHT
+	TOP
+	DOWN
+)
+
+func (m Movement) String() string {
+	return [...]string{"None", "Left", "Right", "Top", "Down"}[m]
+}
+
+
