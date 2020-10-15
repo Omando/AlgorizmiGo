8361a7db5c81fb1f25ffe8ad3ebf61b802fa8e10
Yazan Diranieh <yazanmd@yahoo.com>
2020-10-15 21:48:48 +0100

    placeholder

--- recursion/swap_linked_list_nodes.go
+++ recursion/swap_linked_list_nodes.go
@@ -1,1 +1,14 @@
 package recursion
+
+import "AlgorizmiGo/linkedLists"
+
+func SwapLinkedListNodes(head *linkedLists.Node) *linkedLists.Node {
+	// Must append a dummy
+	var dummy *linkedLists.Node = &linkedLists.Node{Value: 0, Next: head}
+	doSwap(dummy, head, head.Next)
+	return dummy.Next
+}
+
+func doSwap(leftleft *linkedLists.Node, left *linkedLists.Node, right *linkedLists.Node) {
+	
+}
