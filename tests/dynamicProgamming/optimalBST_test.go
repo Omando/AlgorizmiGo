9a828cf80c43f60da08356f192b6d8ec96251d63
Yazan Diranieh <yazanmd@yahoo.com>
2020-10-28 01:16:17 +0000

    optimal binary search tree - delta change 1

--- tests/dynamicProgamming/optimalBST_test.go
+++ tests/dynamicProgamming/optimalBST_test.go
@@ -1,1 +1,44 @@
 package dynamicProgamming
+
+import (
+	"AlgorizmiGo/dynamicProgramming"
+	"fmt"
+	"testing"
+)
+
+func should_calcualte_optimal_costs_and_roots(t *testing.T) {
+
+	tests := []struct {
+		name string
+		keys []byte
+		frequencies []int
+		expectedCosts [][]int
+		expectedRoots [][]int
+	}{
+		{
+			name: 		   "5 Keys",
+			keys:          []byte{'A','B','C','D','E'},
+			frequencies:   []int{25,20,5,20,30},
+			expectedCosts: [][]int {
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+			},
+			expectedRoots: [][]int {
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			costs, roots := dynamicProgramming.FindOptimalBST(test.keys, test.frequencies)
+			fmt.Println(costs, roots);
+		})
+	}
+}
