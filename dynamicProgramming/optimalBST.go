1e51c5b8873ecd3b598ecb92f0f83fea6ca7bca0
Yazan Diranieh <yazanmd@yahoo.com>
2020-10-29 01:19:50 +0000

    optimal binary search tree - delta change 2

--- dynamicProgramming/optimalBST.go
+++ dynamicProgramming/optimalBST.go
@@ -1,7 +1,13 @@
 package dynamicProgramming
 
-func FindOptimalBST(keys []byte, freq []int) (costs [][]int, roots [][]int) {
+import "errors"
 
+func FindOptimalBST(keys []byte, freq []int) (costs [][]int, roots [][]int, err error) {
+	// keys and freq table should bath have the same length
+	if len(keys) != len(freq) {
+		return nil, nil, errors.New("keys and freq arrays should have equal lengths");
+	}
+	
 	// todo
-	return costs, roots
+	return costs, roots, nil
 }
