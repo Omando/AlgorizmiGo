84503cad6638e550da7769734010e51322326a34
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-03 22:59:11 +0000

    recursion - pascal triangle - unit test fixes

--- tests/dynamicProgamming/optimalBST_test.go
+++ tests/dynamicProgamming/optimalBST_test.go
@@ -6,37 +6,37 @@ import (
 	"testing"
 )
 
-func should_calcualte_optimal_costs_and_roots(t *testing.T) {
+func Test_should_calcualte_optimal_costs_and_roots(t *testing.T) {
 	tests := []struct {
-		name string
-		keys []byte
-		frequencies []int
+		name          string
+		keys          []byte
+		frequencies   []int
 		expectedCosts [][]int
 		expectedRoots [][]int
 	}{
 		{
-			name: 		   "5 Keys",
-			keys:          []byte{'A','B','C','D','E'},
-			frequencies:   []int{25,20,5,20,30},
-			expectedCosts: [][]int {
-				{0,0,0,0,0},
-				{0,0,0,0,0},
-				{0,0,0,0,0},
-				{0,0,0,0,0},
-				{0,0,0,0,0},
+			name:        "5 Keys",
+			keys:        []byte{'A', 'B', 'C', 'D', 'E'},
+			frequencies: []int{25, 20, 5, 20, 30},
+			expectedCosts: [][]int{
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
 			},
-			expectedRoots: [][]int {
-				{0,0,0,0,0},
-				{0,0,0,0,0},
-				{0,0,0,0,0},
-				{0,0,0,0,0},
-				{0,0,0,0,0},
+			expectedRoots: [][]int{
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0},
 			},
 		},
 	}
 	for _, test := range tests {
 		costs, roots, err := dynamicProgramming.FindOptimalBST(test.keys, test.frequencies)
-		assert.NotNil(t,err)
+		assert.NotNil(t, err)
 		assert.EqualValues(t, costs, test.expectedCosts)
 		assert.EqualValues(t, roots, test.expectedRoots)
 	}
