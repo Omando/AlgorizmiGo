6c8bbcca8a70fd4a3fc04600eae95fe5178fe46e
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-04 00:44:22 +0000

    optimal binary search tree - unit test fixes

--- tests/dynamicProgamming/optimalBST_test.go
+++ tests/dynamicProgamming/optimalBST_test.go
@@ -19,24 +19,24 @@ func Test_should_calcualte_optimal_costs_and_roots(t *testing.T) {
 			keys:        []byte{'A', 'B', 'C', 'D', 'E'},
 			frequencies: []int{25, 20, 5, 20, 30},
 			expectedCosts: [][]int{
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
+				{25,65,80,125,210},
+				{0, 20, 30, 75, 135},
+				{0, 0, 5, 30, 85},
+				{0, 0, 0, 20, 70},
+				{0, 0, 0, 0, 30},
 			},
 			expectedRoots: [][]int{
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0},
+				{0, 0, 0, 1, 1},
+				{0, 1, 1, 1, 3},
+				{0, 0, 2, 3, 4},
+				{0, 0, 0, 3, 4},
+				{0, 0, 0, 0, 4},
 			},
 		},
 	}
 	for _, test := range tests {
 		costs, roots, err := dynamicProgramming.FindOptimalBST(test.keys, test.frequencies)
-		assert.NotNil(t, err)
+		assert.Nil(t, err)
 		assert.EqualValues(t, costs, test.expectedCosts)
 		assert.EqualValues(t, roots, test.expectedRoots)
 	}
