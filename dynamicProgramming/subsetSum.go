6f1ccf3f1a712d76b8da85e56838be9cbe479ec2
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-05 22:05:04 +0000

    dynamic programming - subset sum delta change 1

--- dynamicProgramming/subsetSum.go
+++ dynamicProgramming/subsetSum.go
@@ -13,7 +13,16 @@ func subsetSum(a []int, B int) ([]int, error) {
 		grid[i] = make([]bool, B+1)
 	}
 
+	// Initialize DP table - first row
+	grid[0][0] = true;
+	for j := 1; j <= B; j++ {
+		grid[0][j] = false;
+	}
 
+	// Initialize DP table - first col
+	for i := 1; i <= len(a); i++ {
+		grid[i][0] = true;
+	}
 
 
 }
