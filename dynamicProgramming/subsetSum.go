8deded2f10ff7afdd63530528a186d67675d2ecf
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-06 22:07:08 +0000

    dynamic programming - subset sum delta change 2

--- dynamicProgramming/subsetSum.go
+++ dynamicProgramming/subsetSum.go
@@ -24,5 +24,23 @@ func subsetSum(a []int, B int) ([]int, error) {
 		grid[i][0] = true;
 	}
 
+	// Populate grid according to S(n,B)=S(n-1,B-an) || S(n-1,B)
+	for i := 1; i <= len(a); i++ {
+		for j := 1; j <= B; j++ {
+			// Get current item
+			 var ai int = a[i-1];    // -1 because we're accessing array 'a' and not array 'grid'
+
+			// If j (eg sum) is less that the current item, then get the result from the cell
+			// above in the previous row, else value = value excluding current number (eg, upper cell)
+			// OR'd with value from previous row where column is the difference between sum and
+			// value
+			if j - ai < 0 {
+				grid[i][j] = false || grid[i-1][j]; // grid[i-1][j - ai] = false from base case
+			} else {
+				grid[i][j] = grid[i-1][j-ai] || grid[i-1][j];
+			}
+		}
+	}
+
 
 }
