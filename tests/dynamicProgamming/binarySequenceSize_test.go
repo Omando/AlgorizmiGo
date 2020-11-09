c0e140b4df71c564c3774926a99db04ae920eee1
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-09 23:42:55 +0000

    dynamic programming - binary sequence size - unit tests

---
+++ tests/dynamicProgamming/binarySequenceSize_test.go
@@ -0,0 +1,24 @@
+package dynamicProgamming
+
+import (
+	"AlgorizmiGo/dynamicProgramming"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_should_calcualte_binary_sequence_size(t *testing.T) {
+	tests := []struct {
+		n            int
+		expectedSize int
+	}{
+		{n: 1, expectedSize: 2},
+		{n: 2, expectedSize: 4},
+		{n: 3, expectedSize: 8},
+	}
+
+	for _, test := range tests {
+		actualSize, err := dynamicProgramming.CalculateBinarySequenceSize(test.n)
+		assert.Nil(t, err)
+		assert.Equal(t, test.expectedSize, actualSize)
+	}
+}
