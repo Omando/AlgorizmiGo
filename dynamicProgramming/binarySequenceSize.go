a7a4576d087f2a0c3aa287dc9ee465bea53d9446
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-10 23:40:19 +0000

    dynamic programming - binary sequence with no consecutive zeros size

--- dynamicProgramming/binarySequenceSize.go
+++ dynamicProgramming/binarySequenceSize.go
@@ -22,3 +22,27 @@ func CalculateBinarySequenceSize(n int) (int, error) {
 	// Return result
 	return table[n], nil
 }
+
+func CalculateNoConsecutiveZerosBinarySequenceSize(n int) (int, error) {
+	// Check invariants
+	if n < 1 {
+		return 0, errors.New("n must be >= 1")
+	}
+
+	// Allocate table
+	var table []int = make([]int, n+1) // cell at index 0 is unused
+
+	// Base conditions
+	table[1] = 2
+	if n > 1 {
+		table[2] = 3
+	}
+
+	// Calculation. Fill table according to recurrence relation T[n] = 2T[n-1]
+	for i := 3; i <= n; i++ {
+		table[i] = table[i-1] + table[i-2]
+	}
+
+	// Return result
+	return table[n], nil
+}
