3e0b5c55462c826760a3d7fc9a3a2ee27e4245f9
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-12 00:51:29 +0000

    dynamic programming - robot movement delta change 2

--- dynamicProgramming/robotMovement.go
+++ dynamicProgramming/robotMovement.go
@@ -17,4 +17,14 @@ func CalculateMovement(n int) (int, error) {
 		grid[i][0] = 1;
 		grid[0][i] = 1;
 	}
+
+	// Calculation. Fill table according to recurrence relation T[x][y] = T[x-1][y] + T[x][y-1]
+	for x := 1; x < n; x++ {
+		for y:= 1; y < n; y++ {
+			grid[x][y] = grid[x-1][y] + grid[x][y-1];
+		}
+	}
+
+	// Return result
+	return grid[n-1][n-1], nil
 }
