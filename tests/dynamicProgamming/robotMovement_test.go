4c7a20794341563965272b60cf54a09763362232
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-14 00:58:03 +0000

    dynamic programming - robot movement - unit test

---
+++ tests/dynamicProgamming/robotMovement_test.go
@@ -0,0 +1,20 @@
+package dynamicProgamming
+
+import (
+	"AlgorizmiGo/dynamicProgramming"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_should_calculate_grid_robot_movement(t *testing.T) {
+	tests := []struct {
+		n            int
+		expectedCount int
+	}{
+		{n: 1, expectedCount: 1},
+		{n: 2, expectedCount: 2},
+		{n: 3, expectedCount: 6},
+		{n: 4, expectedCount: 20},
+		{n: 5, expectedCount: 70},
+	}
+}
