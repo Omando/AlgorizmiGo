ad35a8a99808bd8d39778149ffa4e662f41bd5cd
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-17 23:00:27 +0000

    dynamic programming - rod cut - top-down approach unit tests

--- tests/dynamicProgamming/rodMaximumProfitCut_test.go
+++ tests/dynamicProgamming/rodMaximumProfitCut_test.go
@@ -20,7 +20,7 @@ func TestRodMaximumProfitCut(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := dynamicProgramming.RodMaximumProfitCut(tt.prices, tt.rodLength); got != tt.want {
+			if got := dynamicProgramming.RodMaximumProfitCut_Recursive(tt.prices, tt.rodLength); got != tt.want {
 				t.Errorf("RodMaximumProfitCut() = %v, want %v", got, tt.want)
 			}
 		})
