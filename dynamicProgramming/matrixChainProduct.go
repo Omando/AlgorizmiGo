71bb977dce9efd820df83932933095d4754ceb98
Yazan Diranieh <yazanmd@yahoo.com>
2020-11-24 23:56:51 +0000

    dynamic programming - matrix chain multiplication - delta change 3

--- dynamicProgramming/matrixChainProduct.go
+++ dynamicProgramming/matrixChainProduct.go
@@ -1,7 +1,5 @@
 package dynamicProgramming
 
-import "math"
-
 // This function assumes that matrix Ai has dimensions pi-1 x pi for i = 1,2, ..., n.
 // Input is an array of matrix dimensions: p0, p1, ..., pn. For example, an input array
 // of size 4 such as {3,4,5,6} mean we have 3 matrices as follows:
@@ -20,6 +18,22 @@ func Calculate(dims []int) int {
 	var C [][]int = make([][]int, n)
 	var allCells []int = make([]int, n*n)
 
+	// Allocating 2D array: loop over the rows, slicing each row from the front of the
+	// remaining allCells slice
+	for i,_ := range C {
+		C[i] = allCells[:n]
+		allCells = allCells[n:]
+	}
 
+
+	// C(i,k)= C(i,j) + C(j+1,k) + RiCjCk
+	for i := 1; i < n; i++ {       // We ignore row 0 and column 0
+		//C(1,2) = C(1,1) + C(2,2) + r1c1c2
+		//C(2,3) = C(2,2) + C(3,3) + r2c2c3
+		//C(3,4) = C(3,3) + C(4,4) + r3c3c4
+		//C(1,3) = min(C(1,1) + C(2,3) + r1c1c3, C(1,2) + C(3,3) + r1c2c3)
+
+	}
+	return C[1][n-1];       // we ignore row 0 and col 0
 }
 
