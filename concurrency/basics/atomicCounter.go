bcd1d92037d91a9ea07492684d4fb83c62360ce1
Yazan Diranieh <yazanmd@yahoo.com>
2020-12-06 22:01:37 +0000

    concurrency - atomic counter

---
+++ concurrency/basics/atomicCounter.go
@@ -0,0 +1,20 @@
+package concurrency_basics
+
+import "sync"
+
+type AtomicCounter struct {
+	count int
+	lock sync.Mutex
+}
+
+func (ac *AtomicCounter) Increment() {
+	ac.lock.Lock()
+	ac.count += 1
+	ac.lock.Unlock()
+}
+
+func (ac *AtomicCounter) Get() int {
+	return ac.count
+}
+
+
