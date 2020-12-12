9d1650c5a4b37f49513ae3163f8e126a520a2d59
Yazan Diranieh <yazanmd@yahoo.com>
2020-12-12 01:56:04 +0000

    main file to run clock server

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-package AlgorizmiGo
+package main
+
+import (
+	"AlgorizmiGo/concurrency/basics/ClockServer"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// main -port 8000 -maxConn 2
+	// Read port number from command line argument
+	var port *int = flag.Int("port", 8000,"-port 1234")
+	var maxConnections *int = flag.Int("maxConn", 2, "-maxConn 2")
+	flag.Parse()
+	fmt.Printf("Listening on port %d. Maximum number of connections =%d \n", *port, *maxConnections)
+
+	ClockServer.Run(*port, *maxConnections)
+}
