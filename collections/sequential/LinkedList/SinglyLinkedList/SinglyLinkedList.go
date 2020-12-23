5cce91d456e3095ff3ae18a9734bce76aa7c6fb6
Yazan Diranieh <yazanmd@yahoo.com>
2020-12-23 23:03:45 +0000

    linked lists - improved performance for Contains

--- collections/sequential/LinkedList/SinglyLinkedList/SinglyLinkedList.go
+++ collections/sequential/LinkedList/SinglyLinkedList/SinglyLinkedList.go
@@ -1,4 +1,4 @@
-package sequential
+package SinglyLinkedList
 
 import (
 	"errors"
@@ -6,8 +6,8 @@ import (
 
 // LinkedListNode node for a singlye linked list
 type LinkedListNode struct {
-	value interface{}			// Any type. No run-time type-safety. Need generics!
-	next *LinkedListNode
+	value interface{}     // Any type. No run-time type-safety. Need generics!
+	next  *LinkedListNode // Must use pointer for recursive types. Otherwise size of struct is unknown to compiler
 }
 
 // SinglyLinkedList singly linked list
@@ -23,7 +23,7 @@ func New() *SinglyLinkedList {
 	// Can also use:
 	//	return &SinglyLinkedList{head: nil, tail: nil, size: 0}
 	//	return (&SinglyLinkedList{}).init()
-	return new(SinglyLinkedList).init()	// same as:
+	return new(SinglyLinkedList).init() // same as:
 }
 
 // Receiver is a pointer which receives a copy of the address. If receiver was value,
@@ -38,19 +38,26 @@ func (list *SinglyLinkedList) init() *SinglyLinkedList {
 
 // First returns the first element of list l or nil if the list is empty.
 func (list *SinglyLinkedList) First() (interface{}, error) {
-	if list.size == 0 { return nil, errors.New("List is empty") }
-	return list.head.value,nil
+	if list.size == 0 {
+		return nil, errors.New("List is empty")
+	}
+	return list.head.value, nil
 }
 
+// Last returns the last element of list l or nil if the list is empty.
 func (list *SinglyLinkedList) Last() (interface{}, error) {
-	if list.size == 0 { return nil, errors.New("List is empty") }
+	if list.size == 0 {
+		return nil, errors.New("List is empty")
+	}
 	return list.tail.value, nil
 }
 
+// Length returns count of elements in the list
 func (list *SinglyLinkedList) Length() int {
 	return list.size
 }
 
+// Prepend adds the given value to the beginning of the list
 func (list *SinglyLinkedList) Prepend(value interface{}) {
 	node := &LinkedListNode{value: value, next: nil}
 	// If the list is empty, head and tail both point to the new node
@@ -58,40 +65,42 @@ func (list *SinglyLinkedList) Prepend(value interface{}) {
 		list.head = node
 		list.tail = node
 	} else {
-		node.next = list.head		// new node points to the current head
-		list.head = node			// new node is the new head
+		node.next = list.head // new node points to the current head
+		list.head = node      // new node is the new head
 	}
 
 	// Update size
 	list.size++
 }
 
-func (list *SinglyLinkedList) Append(value interface{}){
+// Append adds the given value to the end of the list
+func (list *SinglyLinkedList) Append(value interface{}) {
 	node := &LinkedListNode{value: value, next: nil}
 	// If the list is empty, head and tail both point to the new node
 	if list.size == 0 {
 		list.head = node
 		list.tail = node
 	} else {
-		list.tail.next = node;
-		list.tail = node;
+		list.tail.next = node
+		list.tail = node
 	}
 	// Update size
 	list.size++
 }
 
-// Remove: returns value of removed node. Panics if value was not found
-func (list* SinglyLinkedList) Remove(value interface{}) (interface{}, error) {
+// Remove deletes the given node if found, and returns value of removed node.
+// Panics if value was not found
+func (list *SinglyLinkedList) Remove(value interface{}) (interface{}, error) {
 	// Iterate until we find a node with the given given value
 	var previous *LinkedListNode
 	for node := list.head; node != nil; node = node.next {
 		if node.value == value {
-			if list.head == node {				// Removing head
+			if list.head == node { // Removing head
 				list.head = list.head.next
-			} else if list.tail == node {		// Removing tail
-				previous.next = nil;
+			} else if list.tail == node { // Removing tail
+				previous.next = nil
 				list.tail = previous
-			} else {							// Everything in between
+			} else { // Everything in between
 				previous.next = node.next
 			}
 
@@ -109,12 +118,23 @@ func (list* SinglyLinkedList) Remove(value interface{}) (interface{}, error) {
 	return nil, errors.New("value not found")
 }
 
+// Contains returns true if given value exists, otherwise returns false
 func (list *SinglyLinkedList) Contains(value interface{}) bool {
-	for node := list.head; node != nil; node = node.next {
+	if list.size == 0 {
+		return false
+	}
+
+	// Check head and tail first
+	if list.head.value == value || list.tail.value == value {
+		return true
+	}
+
+	// Iterate through the rest of the list
+	for node := list.head.next; node != list.tail; node = node.next {
 		if node.value == value {
 			return true
 		}
 	}
+
 	return false
 }
-
