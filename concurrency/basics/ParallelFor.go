337dad1a2ef1f36d5f18dc9504ad96ca6e25a565
Yazan Diranieh <yazanmd@yahoo.com>
2020-12-31 01:34:13 +0000

    concurrency - initial implementation for parallel for

--- concurrency/basics/ParallelFor.go
+++ concurrency/basics/ParallelFor.go
@@ -4,6 +4,11 @@ import "sync"
 
 type ActionFunc = func(data interface{}) interface{}
 
+// ParallelFor Starting implementation of a parallel for with a given function type
+// This initial implementation is a place holder and will be enhanced/modified
+// as required
+// Enhancement 1: provide updates to client via a channel
+// Enhancement 2: parameterize the return type
 func ParallelFor(data []interface{}, action ActionFunc) (results []interface{}) {
 	var wg sync.WaitGroup
 	var lock sync.Mutex
