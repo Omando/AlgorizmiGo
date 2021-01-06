be2bef5fe01430bb87130b781d8ebb45269fc537
Yazan Diranieh <yazanmd@yahoo.com>
2021-01-06 16:24:00 +0000

    sequential web crawler - completed

--- concurrency/basics/WebCrawler/webCrawler_Sequential.go
+++ concurrency/basics/WebCrawler/webCrawler_Sequential.go
@@ -1,11 +1,41 @@
 package WebCrawler
 
 import (
+	"AlgorizmiGo/collections/sequential"
 	"fmt"
-	"golang.org/x/net/html" // Use cmd: go get golang.org/x/net
 	"log"
 	"net/http"
+	"golang.org/x/net/html"		// Use cmd: go get golang.org/x/net
 )
+
+// Web crawling using breadth-first traversal
+func Crawl(topUrl string) {
+	// This map is used to ensure that we do not crawl the same url twice
+	processed := make(map[string]bool)
+
+	// Used in breadth traversal
+	var queue = sequential.CreateQueue()
+	queue.Enqueue(topUrl)
+
+	for !queue.IsEmpty() {
+		item, err  := queue.Dequeue()
+		if err != nil {
+			fmt.Printf("Failed to queue item: %v", err)
+			continue
+		}
+
+		// Crawl this url and return a list of new urls to crawl
+		url := item.(string)
+		links := crawlUrl(url)
+		processed[url] = true
+		for _, link := range links {
+			// Queue this link if not processed before
+			if !processed[link] {
+				queue.Enqueue(link)
+			}
+		}
+	}
+}
 
 // A thin wrapper around extract to get and return links found in a URL
 func crawlUrl(url string) (links []string) {
