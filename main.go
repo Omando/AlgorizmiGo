4079b7ba18bce20c762094d5a21f3b8367fcde0d
Yazan Diranieh <yazanmd@yahoo.com>
2021-01-21 23:39:24 +0000

    Calls to latest modules (web crawler, dir walker, etc)

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"AlgorizmiGo/concurrency/basics/EchoServer"
+	"AlgorizmiGo/concurrency/basics/chatServer"
 	"flag"
 	"fmt"
 )
@@ -9,11 +9,22 @@ import (
 func main() {
 	// To run: go run main.go -port 2222 -maxConn 2
 	// Read port number from command line argument
-	var port *int = flag.Int("port", 8000,"-port 1234")
+	var port *int = flag.Int("port", 8000, "-port 1234")
 	var maxConnections *int = flag.Int("maxConn", 2, "-maxConn 2")
 	flag.Parse()
 	fmt.Printf("Listening on port %d. Maximum number of connections =%d \n", *port, *maxConnections)
 
-	//ClockServer.Run(*port, *maxConnections)
-	EchoServer.Run(*port, *maxConnections)
+	//clockServer.Run(*port, *maxConnections)
+	//echoServer.Run(*port, *maxConnections)
+	//pipelines.Run()
+	//pipelines.RunWithRange()
+	//pipelines.RunConcurrentStages()
+	//webCrawler.CrawlSequentially("https://golang.org")
+	//webCrawler.CrawlConcurrently("https://golang.org")
+	//counters.AbortableCountdown(10);
+	//diretoryTraversal.TraverseRoots([]string{"C:\\Temp"})
+	//diretoryTraversal.TraverseRootsAndMultiplexResults([]string{"C:\\Temp"})
+	chatServer.Run(*port)
+
+	fmt.Println("Completed")
 }
