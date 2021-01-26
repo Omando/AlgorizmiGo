accc10a975791fd33f9bb1a647ff89fad3edec38
Yazan Diranieh <yazanmd@yahoo.com>
2021-01-26 23:17:44 +0000

    concurrent runner for sequential cache - has data races

--- concurrency/basics/nonBlockingCache/runner.go
+++ concurrency/basics/nonBlockingCache/runner.go
@@ -3,6 +3,7 @@ package nonBlockingCache
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -24,4 +25,32 @@ func RunSequential(){
 		}
 		fmt.Printf("%s [%s]: %d bytes\n", url, time.Since(start), len(value.([]byte)))
 	}
+}
+
+func RunConcurrent(){
+	var urls = []string {
+		"https://golang.org",
+		"https://godoc.org",
+		"https://play.golang.org",
+		"https://godoc.org",
+		"https://play.golang.org",
+	}
+
+	var wg sync.WaitGroup
+	memo := New(HttpGetBody)
+	for _, url := range urls {
+		wg.Add(1)
+		go func(link string) {
+			start := time.Now();
+			value, err := memo.Get(link)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%s [%s]: %d bytes\n", link, time.Since(start), len(value.([]byte)))
+			wg.Done()
+		}(url)
+	}
+
+	// Wait until all urls have run
+	wg.Wait()
 }
