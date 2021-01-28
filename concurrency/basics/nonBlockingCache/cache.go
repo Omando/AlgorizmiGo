b16c0d34d57913e07e1d1aa38d57d233aab1bb6e
Yazan Diranieh <yazanmd@yahoo.com>
2021-01-28 23:51:30 +0000

    Adding concurrent lock-free implementation

--- concurrency/basics/nonBlockingCache/cache.go
+++ concurrency/basics/nonBlockingCache/cache.go
@@ -1,9 +1,16 @@
 package nonBlockingCache
 
+import "sync"
+
 // result encapsulates the return value of httpGetBody
 type result struct {
 	value interface{}
 	err error
+}
+
+type entry struct {
+	res result
+	ready chan struct{}		// closed when res is ready
 }
 
 // Func is the type of the function whose result will be memoized
@@ -12,7 +19,8 @@ type Func func(key string) (interface{}, error)
 // Memo caches the results of calling a Func
 type Memo struct {
 	f Func
-	cache map[string]result
+	cache map[string]*entry
+	lock sync.Mutex
 }
 
 // Create an empty cache for a Func
@@ -21,16 +29,10 @@ type Memo struct {
 func New(f Func) *Memo {
 	return &Memo{
 		f:     f,
-		cache: make(map[string]result),
+		cache: make(map[string]*entry),
 	}
 }
 
 func (memo *Memo) Get(key string) (interface{}, error) {
-	result, ok := memo.cache[key]
-	if  !ok {
-		// Invoke the function lazily and cache its value
-		result.value, result.err = memo.f(key);
-		memo.cache[key] = result
-	}
-	return result.value, result.err
+
 }
